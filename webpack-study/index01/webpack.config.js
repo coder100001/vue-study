7cffb5312b9f710192e9a9c522757cd6a1242d08
yangyiyang_dxm <yangyiyang@duxiaoman.com>
2021-03-01 15:14:07 +0800

    fix

--- webpack-study/index01/webpack.config.js
+++ webpack-study/index01/webpack.config.js
@@ -1,9 +1,9 @@
- const path = require('path')
+ const path = require('path');
 
  module.exports = {
      entry: "./src/main.js",
      output: {
-         path: "./dist",
+         path: path.resolve(__dirname, "dist"),
          filename: "bundle.js",
      },
  };
