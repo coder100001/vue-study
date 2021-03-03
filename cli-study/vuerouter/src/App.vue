fe1423d1defa81d892c0399f629fc7d649317ed3
yangyiyang_dxm <yangyiyang@duxiaoman.com>
2021-03-03 19:47:11 +0800

    fix

--- cli-study/vuerouter/src/App.vue
+++ cli-study/vuerouter/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <router-link to="/home">home</router-link>
-    <router-link to="/account">account</router-link>
-    <router-link :to="'/user/'+userId">user</router-link>
+    <div id="tab-bar">
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -10,27 +14,28 @@
 <script>
 export default {
   name: 'App',
-  methods: {
-    homeClick: function() {
-      this.$router.push('/home');
-    },
-    accountClick: function() {
-      this.$router.push('/account');
-    },
-    userClick: function() {
-      this.$router.push('/user/:userId');
-    }
-  },
-  data() {
-    return {
-      userId: 1001,
-    }
+  components: {
+
   }
 }
 </script>
 
 <style>
-  .router-link-active {
-    color: red;
+  @import "./assets/css/base.css";  
+
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    box-shadow: 0 -3px 1px rgba(100, 100, 100, 0.01);
   }
 </style>
