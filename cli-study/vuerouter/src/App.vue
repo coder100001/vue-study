70de12d3156cb959189a664ce5c3004b20ca453a
liunian <1412939446@qq.com>
2021-03-03 22:29:51 +0800

    fix

--- cli-study/vuerouter/src/App.vue
+++ cli-study/vuerouter/src/App.vue
@@ -1,42 +1,20 @@
 <template>
   <div id="app">
-    <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-
-    </div>
+    <tab-bar></tab-bar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import TabBar from './components/tabbar/TabBar.vue'
 export default {
   name: 'App',
   components: {
-
+    TabBar
   }
 }
 </script>
 
 <style>
   @import "./assets/css/base.css";  
-
-  #tab-bar {
-    display: flex;
-    background-color: #f6f6f6;
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
-
-  .tab-bar-item {
-    flex: 1;
-    text-align: center;
-    height: 49px;
-    box-shadow: 0 -3px 1px rgba(100, 100, 100, 0.1);
-  }
-
 </style>
