12aee8d9a3f68c13415852eea09b64f6451384d0
yangyiyang_dxm <yangyiyang@duxiaoman.com>
2021-03-04 14:44:44 +0800

    fix

--- cli-study/vuerouter/src/App.vue
+++ cli-study/vuerouter/src/App.vue
@@ -1,40 +1,17 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <tab-bar>
-      <tab-bar-item path="/home">
-        <img slot="item-icon" src="./assets/img/tabBar/2fe7ffc2c0894422a2c2ab3cf.png" alt="">
-        <img slot="item-icon-actived" src="./assets/img/tabBar/home_actived.png" alt="">
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category">
-        <img slot="item-icon" src="./assets/img/tabBar/4e527425a7f62e825d49c42ae.png" alt="">
-        <img slot="item-icon-actived" src="./assets/img/tabBar/cate_actived.png" alt="">
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart">
-        <img slot="item-icon" src="./assets/img/tabBar/886d841df3a2f2ba26db5eabe.png" alt="">
-        <img slot="item-icon-actived" src="./assets/img/tabBar/shop_car_actived.png" alt="">
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile">
-        <img slot="item-icon" src="./assets/img/tabBar/0e43a67a4c7d46c6c4a8463d7.png" alt="">
-        <img slot="item-icon-actived" src="./assets/img/tabBar/mine_actived.png" alt="">
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from './components/tabbar/TabBar.vue'
-import TabBarItem from './components/tabbar/TabBarItem.vue'
+import MainTabBar from './components/MainTabBar'
 
 export default {
   name: 'App',
   components: {
-    TabBar,
-    TabBarItem,
+    MainTabBar,
   }
 }
 </script>
---
+++ cli-study/vuerouter/src/components/MainTabBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <tab-bar>
+      <tab-bar-item path="/home">
+        <img slot="item-icon" src="../assets/img/tabBar/2fe7ffc2c0894422a2c2ab3cf.png" alt="">
+        <img slot="item-icon-actived" src="../assets/img/tabBar/home_actived.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img slot="item-icon" src="../assets/img/tabBar/4e527425a7f62e825d49c42ae.png" alt="">
+        <img slot="item-icon-actived" src="../assets/img/tabBar/cate_actived.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img slot="item-icon" src="../assets/img/tabBar/886d841df3a2f2ba26db5eabe.png" alt="">
+        <img slot="item-icon-actived" src="../assets/img/tabBar/shop_car_actived.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img slot="item-icon" src="../assets/img/tabBar/0e43a67a4c7d46c6c4a8463d7.png" alt="">
+        <img slot="item-icon-actived" src="../assets/img/tabBar/mine_actived.png" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from './tabbar/TabBar'
+import TabBarItem from './tabbar/TabBarItem'
+
+export default {
+    name: 'MainTabBar',
+    components: {
+      TabBar,
+      TabBarItem,
+    }
+}
+</script>
